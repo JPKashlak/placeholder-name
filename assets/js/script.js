cbc7fad6af19bb6161577dab18df78807f103665
flokamp <74511633+flokamp@users.noreply.github.com>
2021-01-23 00:40:35 -0600

    hide cards until user searches to avoid empty card state

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,31 @@ var errorMsg =  document.getElementById('error');
 var covidCardTitle = document.getElementById("covid");
 var repCardTitle =  document.getElementById('rep');
 
-// show error message if incorrect zipcode input
 function validateForm() {
-  var charCount = JSON.stringify(userInput.value).length - 2;
   
+  // show error message if incorrect zipcode input
+  var charCount = JSON.stringify(userInput.value).length - 2;
+
   if (charCount != 5 || NaN) {
     userInput.classList.add("is-danger");
     errorMsg.innerHTML="Zip code must be 5 digits";
+  } else {
+    displayData();
   }
+}
 
-  else {
+function displayData() {
 
-    userZip = JSON.stringify(userInput.value);
-    fetch("http://dev.virtualearth.net/REST/v1/Locations?postalCode=" + userZip + "&key=Ag9vSCbKCVavmpm_CAS77TmHeRGxbmAxECOfwknIrua4eOT9rwT4ifxTOuwC9-V0").then(function(response) {
-      response.json().then(function(data) {
-        var countyName = data.resourceSets[0].resources[0].address.adminDistrict2;
-        covidCardTitle.innerHTML= countyName + " Covid Data";
-        repCardTitle.innerHTML= countyName + " Politican Info";
-      });
+  // use the user input to return the county name
+  userZip = JSON.stringify(userInput.value);
+  fetch("http://dev.virtualearth.net/REST/v1/Locations?postalCode=" + userZip + "&key=Ag9vSCbKCVavmpm_CAS77TmHeRGxbmAxECOfwknIrua4eOT9rwT4ifxTOuwC9-V0").then(function(response) {
+    response.json().then(function(data) {
+      var countyName = data.resourceSets[0].resources[0].address.adminDistrict2;
+      covidCardTitle.innerHTML= countyName + " Covid Data";
+      repCardTitle.innerHTML= countyName + " Politican Info";
+
+  // show the covid and politican cards
+  $("div").removeClass("hidden");
     });
-  }
-  
+  });
 }
