54752eb73ca4f7a9c54fcfc9df58a1054e2df34b
jpkashlak <jpkashlak@gmail.com>
2021-01-23 15:37:26 -0600

    linked fetch requests into one another

--- assets/js/congress-script.js
+++ assets/js/congress-script.js
@@ -1,21 +1,40 @@
-var state = "pa"
+// var zipSubmit = document.querySelector("#searchBtn");
+var zipInput = document.querySelector("#inputForm")
+var zipEntry = document.querySelector("#zipEntry")
 
-var test = "https://cors-anywhere.herokuapp.com/https://v3.openstates.org/people?jurisdiction=" + state + "&page=1&per_page=20&apikey=72659b34-798d-4441-b6ee-c86ef9973ebb"
 
 
-fetch(test).then(function(response) {
-    response.json().then(function(data) {
-        console.log(data.results)
-        for (var i = 0; i < data.results.length; i++) {
-            console.log(data.results[i].name)
-        }
+
+
+var pullPoliticData = function(event) {
+    var repData = zipEntry.value.trim();
+    console.log(repData);
+    event.preventDefault();
+    
+    var testZip = "http://dev.virtualearth.net/REST/v1/Locations?postalCode=" + repData + "&key=Ag9vSCbKCVavmpm_CAS77TmHeRGxbmAxECOfwknIrua4eOT9rwT4ifxTOuwC9-V0"
+
+    fetch(testZip).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data)
+            console.log(data.resourceSets[0].resources[0].address.adminDistrict2, data.resourceSets[0].resources[0].address.adminDistrict)
+            
+            var state = data.resourceSets[0].resources[0].address.adminDistrict
+    
+
+    var testPolitic = "https://cors-anywhere.herokuapp.com/https://v3.openstates.org/people?jurisdiction=" + state + "&page=1&per_page=5&apikey=72659b34-798d-4441-b6ee-c86ef9973ebb"
+
+    fetch(testPolitic).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data.results)
+            for (var i = 0; i < data.results.length; i++) {
+                console.log(data.results[i].name, data.results[i].party, data.results[i].extras.facebook)
+            }
+        })
+    }) 
+
+        })
     })
-})
 
+}
 
-
-
-
-
-
-// 72659b34-798d-4441-b6ee-c86ef9973ebb
+zipInput.addEventListener("submit", pullPoliticData)
