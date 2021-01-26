40f1121ad83f0135acdc571b4be949f2c5fcf410
jpkashlak <jpkashlak@gmail.com>
2021-01-25 20:13:10 -0600

    merged script & congress-script

--- assets/js/congress-script.js
+++ assets/js/congress-script.js
@@ -1,92 +1,92 @@
-var zipInput = document.querySelector("#inputForm")
-var zipEntry = document.querySelector("#zip-field")
+// var zipInput = document.querySelector("#inputForm")
+// var zipEntry = document.querySelector("#zip-field")
 
 
-var pullPoliticData = function(event) {
+// var pullPoliticData = function(event) {
 
-    var localZip = zipEntry.value.trim();
-    console.log(localZip);
-    event.preventDefault();
+//     var localZip = zipEntry.value.trim();
+//     console.log(localZip);
+//     event.preventDefault();
     
-    var zipLat = "https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/3gViGaWo0FXAWpATeUncO3LrgOb7olNRh6WyOq1qm7n1rvBkW5QEdo7ajb8GqS3o/info.json/" + localZip + "/degrees"
-        fetch(zipLat).then(function(response) {
-            response.json().then(function(data) {
-                console.log(data)
-                var localLat = data.lat 
-                var localLong = data.lng
+//     var zipLat = "https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/3gViGaWo0FXAWpATeUncO3LrgOb7olNRh6WyOq1qm7n1rvBkW5QEdo7ajb8GqS3o/info.json/" + localZip + "/degrees"
+//         fetch(zipLat).then(function(response) {
+//             response.json().then(function(data) {
+//                 console.log(data)
+//                 var localLat = data.lat 
+//                 var localLong = data.lng
     
-        var findRep = "https://v3.openstates.org/people.geo?lat=" + localLat + "&lng=" + localLong + "&apikey=72659b34-798d-4441-b6ee-c86ef9973ebb"    
-            fetch(findRep).then(function(response) {
-                return response.json().then(function(data) {
-                    console.log(data)
+//         var findRep = "https://v3.openstates.org/people.geo?lat=" + localLat + "&lng=" + localLong + "&apikey=72659b34-798d-4441-b6ee-c86ef9973ebb"    
+//             fetch(findRep).then(function(response) {
+//                 return response.json().then(function(data) {
+//                     console.log(data)
 
-                // Create Card
+//                 // Create Card
                        
-                    // for (var i = 0; i < data.results.length; i++) {
+//                     // for (var i = 0; i < data.results.length; i++) {
 
-                        // List Portrait
-                        var imageUrl = data.results[0].image
-                        var photoSlot = document.querySelector("#portrait")
-                        var portrait = document.createElement("img")
-                            portrait.src = imageUrl                 
-                            photoSlot.style.width = "150px"
-                            photoSlot.style.height = "200px"        
-                            photoSlot.appendChild(portrait)
-                            if (photoSlot.childNodes.length > 1) {
-                                photoSlot.removeChild(photoSlot.childNodes[0])
-                            }
+//                         // List Portrait
+//                         var imageUrl = data.results[0].image
+//                         var photoSlot = document.querySelector("#portrait")
+//                         var portrait = document.createElement("img")
+//                             portrait.src = imageUrl                 
+//                             photoSlot.style.width = "150px"
+//                             photoSlot.style.height = "200px"        
+//                             photoSlot.appendChild(portrait)
+//                             if (photoSlot.childNodes.length > 1) {
+//                                 photoSlot.removeChild(photoSlot.childNodes[0])
+//                             }
 
-                        // List Name
-                        var fetchName = data.results[0].current_role.title + " " + data.results[0].name + " (District " + data.results[0].current_role.district + ")"             
-                        var nameSlot = document.querySelector("#nameData")
-                            nameSlot.textContent = fetchName
-                        if (data.results[0].party === "Democratic") {
-                            nameSlot.style.color = "#3636b3"
-                        }
-                        else if (data.results[0].party === "Republican") {
-                            nameSlot.style.color = "#c70909"
-                        }
-                        else {
-                            nameSlot.style.color = "#000000"
-                        }
-                        // List Contact
-                        var webSlot = document.querySelector("#contactData")
-                        if (data.results[0].current_role.title === "Representative") {
-                            webSlot.setAttribute("href", data.results[0].openstates_url)
-                            webSlot.textContent = "Contact your " + data.results[0].current_role.title
-                        }
-                        else if (data.results[0].current_role.title === "Senator") {
-                            webSlot.setAttribute("href", data.results[0].openstates_url)
-                            webSlot.textContent = "Contact your " + data.results[0].current_role.title 
-                        }
-                        else if (data.results[0].current_role.title === "Delegate") {
-                            webSlot.setAttribute("href", data.results[0].openstates_url)
-                            webSlot.textContent = "Contact your " + data.results[0].current_role.title
-                        }
-                        else {
-                            webSlot.setAttribute("href", data.results[0].openstates_url)
-                            webSlot.textContent = "Contact!"
-                        }
+//                         // List Name
+//                         var fetchName = data.results[0].current_role.title + " " + data.results[0].name + " (District " + data.results[0].current_role.district + ")"             
+//                         var nameSlot = document.querySelector("#nameData")
+//                             nameSlot.textContent = fetchName
+//                         if (data.results[0].party === "Democratic") {
+//                             nameSlot.style.color = "#3636b3"
+//                         }
+//                         else if (data.results[0].party === "Republican") {
+//                             nameSlot.style.color = "#c70909"
+//                         }
+//                         else {
+//                             nameSlot.style.color = "#000000"
+//                         }
+//                         // List Contact
+//                         var webSlot = document.querySelector("#contactData")
+//                         if (data.results[0].current_role.title === "Representative") {
+//                             webSlot.setAttribute("href", data.results[0].openstates_url)
+//                             webSlot.textContent = "Contact your " + data.results[0].current_role.title
+//                         }
+//                         else if (data.results[0].current_role.title === "Senator") {
+//                             webSlot.setAttribute("href", data.results[0].openstates_url)
+//                             webSlot.textContent = "Contact your " + data.results[0].current_role.title 
+//                         }
+//                         else if (data.results[0].current_role.title === "Delegate") {
+//                             webSlot.setAttribute("href", data.results[0].openstates_url)
+//                             webSlot.textContent = "Contact your " + data.results[0].current_role.title
+//                         }
+//                         else {
+//                             webSlot.setAttribute("href", data.results[0].openstates_url)
+//                             webSlot.textContent = "Contact!"
+//                         }
                             
-                        //List Facebook
-                        var fetchFacebook = data.results[0].extras.facebook
-                        var facebookSlot = document.querySelector("#facebookData")
-                        if (data.results[0].extras.facebook) {
-                            facebookSlot.textContent = fetchFacebook
-                            facebookSlot.setAttribute("href", fetchFacebook)
-                        }
-                        else {
-                            facebookSlot.textContent = "N/A"
-                            facebookSlot.setAttribute("href", "")
-                        }
-                    // }
-                })
-            })
-        })
-    }) 
-}
+//                         //List Facebook
+//                         var fetchFacebook = data.results[0].extras.facebook
+//                         var facebookSlot = document.querySelector("#facebookData")
+//                         if (data.results[0].extras.facebook) {
+//                             facebookSlot.textContent = fetchFacebook
+//                             facebookSlot.setAttribute("href", fetchFacebook)
+//                         }
+//                         else {
+//                             facebookSlot.textContent = "N/A"
+//                             facebookSlot.setAttribute("href", "")
+//                         }
+//                     // }
+//                 })
+//             })
+//         })
+//     }) 
+// }
 
-zipInput.addEventListener("submit", pullPoliticData)
+// zipInput.addEventListener("submit", pullPoliticData)
 
 
 
