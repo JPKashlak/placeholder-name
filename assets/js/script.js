d73ffe43009ae3d1f53272b0d8da9079adb54cd5
flokamp <74511633+flokamp@users.noreply.github.com>
2021-01-27 14:03:56 -0600

    making sure master is up to date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,39 @@
 var config = {
-  virtualEarthKey: "Ag9vSCbKCVavmpm_CAS77TmHeRGxbmAxECOfwknIrua4eOT9rwT4ifxTOuwC9-V0",
-  zipCodeKey1: "3gViGaWo0FXAWpATeUncO3LrgOb7olNRh6WyOq1qm7n1rvBkW5QEdo7ajb8GqS3o",
-  openStatesKey: "72659b34-798d-4441-b6ee-c86ef9973ebb"
+  virtualEarthKey: "Ag9vSCbKCVavmpm_CAS77TmHeRGxbmAxECOfwknIrua4eOT9rwT4ifxTOuwC9-V0"
+  , zipCodeKey1: "3gViGaWo0FXAWpATeUncO3LrgOb7olNRh6WyOq1qm7n1rvBkW5QEdo7ajb8GqS3o"
+  , openStatesKey: "72659b34-798d-4441-b6ee-c86ef9973ebb"
 }
 
 
-var repName =  document.getElementById('name-data');
-var repTitle =  document.getElementById('role-data');
+var repName = document.getElementById('name-data');
+var repTitle = document.getElementById('role-data');
 var zipInput = document.querySelector("#inputForm");
-var savedZips =  document.getElementById('saved-zips');
+var savedZips = document.getElementById('saved-zips');
 var zipEntry = document.querySelector("#zip-field");
 var errorMsg = document.getElementById('error')
 var virtualEarthKey = config.virtualEarthKey;
 var zipCodeKey1 = config.zipCodeKey1;
 var openStatesKey = config.openStatesKey;
 let currentZips
-  if (localStorage.getItem('zip')) {
-    currentZips = JSON.parse(localStorage.getItem('zip'))
-  } else {
-    currentZips = []
-  }
+if (localStorage.getItem('zip')) {
+  currentZips = JSON.parse(localStorage.getItem('zip'))
+} else {
+  currentZips = []
+}
 
 // display data takes argument for user zip localStorage.setItem(userZip)
 
 // get valid zip code from user
 function validateForm() {
-  
-  var charCount = JSON.stringify(zipEntry.value).length - 2;
+
+  var charCount = JSON.stringify(zipEntry.value)
+      .length - 2;
 
   if (charCount != 5 || NaN) {
-    zipEntry.classList.add("is-danger");
-    errorMsg.innerHTML="Zip code must be 5 digits";
+      zipEntry.classList.add("is-danger");
+      errorMsg.innerHTML = "Zip code must be 5 digits";
   } else {
-    displayData();
+      displayData();
   }
 }
 
@@ -42,8 +43,9 @@ function displayData() {
   // if zips exist, append to array
   currentZips.push(localZip)
   localStorage.setItem("zip", JSON.stringify(currentZips))
-  // show the covid and politican cards
-  $("div").removeClass("hidden");
+      // show the covid and politican cards
+  $("div")
+      .removeClass("hidden");
   storeZips();
 
 };
@@ -60,81 +62,83 @@ function storeZips() {
 function loadZips() {
   // loop through savedTasks array
   for (var i = 0; i < currentZips.length; i++) {
-    // pass each task object into the `createTaskEl()` function
-    newZipEl = document.createElement('a');
-    newZipEl.innerHTML = "<i class='fas fa-map-marker-alt'></i> " + currentZips[i];
-    newZipEl.setAttribute('class', 'tag');
-    newZipEl.setAttribute('href', 'http://google.com');
-    savedZips.appendChild(newZipEl);
+      // pass each task object into the `createTaskEl()` function
+      newZipEl = document.createElement('a');
+      newZipEl.innerHTML = "<i class='fas fa-map-marker-alt'></i> " + currentZips[i];
+      newZipEl.setAttribute('class', 'tag');
+      newZipEl.setAttribute('href', 'http://google.com');
+      savedZips.appendChild(newZipEl);
 
   }
 }
-   
 
 
-var pullPoliticData = function(event) {
 
-    var localZip = zipEntry.value.trim();
-    console.log(localZip);
-    event.preventDefault();
-    
-    var zipLat = "https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/" + zipCodeKey1 + "/info.json/" + localZip + "/degrees"
-        fetch(zipLat).then(function(response) {
-            response.json().then(function(data) {
-                console.log(data)
-                var localLat = data.lat 
-                var localLong = data.lng
-    
-        var findRep = "https://v3.openstates.org/people.geo?lat=" + localLat + "&lng=" + localLong + "&apikey=" + openStatesKey    
-            fetch(findRep).then(function(response) {
-                return response.json().then(function(data) {
-                    console.log(data)
+var pullPoliticData = function (event) {
 
-                // List Portrait
-                var imageUrl = data.results[0].image
-                var avatar = document.querySelector("img")
-                    avatar.src = imageUrl                 
-                            
-                // List Name
-                var fetchName = data.results[0].current_role.title + " " + data.results[0].name           
-                repName.innerHTML= fetchName;
+  var localZip = zipEntry.value.trim();
+  console.log(localZip);
+  event.preventDefault();
 
-                // List Title
-                var fetchTitle = "District " + data.results[0].current_role.district       
-                repTitle.innerHTML= fetchTitle;
-                        
-                // List Contact
-                $("contact-data").removeClass("hidden");
-                var webSlot = document.getElementById("contact-link")
-                if (data.results[0].current_role.title === "Representative") {
-                    webSlot.setAttribute("href", data.results[0].openstates_url)
-                    webSlot.textContent = "Contact your " + data.results[0].current_role.title
-                }
-                else if (data.results[0].current_role.title === "Senator") {
-                    webSlot.setAttribute("href", data.results[0].openstates_url)
-                    webSlot.textContent = "Contact your " + data.results[0].current_role.title 
-                }
-                else if (data.results[0].current_role.title === "Delegate") {
-                    webSlot.setAttribute("href", data.results[0].openstates_url)
-                    webSlot.textContent = "Contact your " + data.results[0].current_role.title
-                }
-                else {
-                    webSlot.setAttribute("href", data.results[0].openstates_url)
-                    webSlot.textContent = "Contact!"
-                }
-                    
-                //List Facebook
+  var zipLat = "https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/" + zipCodeKey1 + "/info.json/" + localZip + "/degrees"
+  fetch(zipLat)
+      .then(function (response) {
+          response.json()
+              .then(function (data) {
+                  console.log(data)
+                  var localLat = data.lat
+                  var localLong = data.lng
 
-                var fetchFacebook = data.results[0].extras.facebook
-                var facebookSlot = document.getElementById("facebook-data")
-                if (data.results[0].extras.facebook) {
-                    facebookSlot.setAttribute("href", fetchFacebook)
-                }
-               
-                })
-            })
-        })
-    }) 
+                  var findRep = "https://v3.openstates.org/people.geo?lat=" + localLat + "&lng=" + localLong + "&apikey=" + openStatesKey
+                  fetch(findRep)
+                      .then(function (response) {
+                          return response.json()
+                              .then(function (data) {
+                                  console.log(data)
+
+                                  // List Portrait
+                                  var imageUrl = data.results[0].image
+                                  var avatar = document.querySelector("img")
+                                  avatar.src = imageUrl
+
+                                  // List Name
+                                  var fetchName = data.results[0].current_role.title + " " + data.results[0].name
+                                  repName.innerHTML = fetchName;
+
+                                  // List Title
+                                  var fetchTitle = "District " + data.results[0].current_role.district
+                                  repTitle.innerHTML = fetchTitle;
+
+                                  // List Contact
+                                  $("contact-data")
+                                      .removeClass("hidden");
+                                  var webSlot = document.getElementById("contact-link")
+                                  if (data.results[0].current_role.title === "Representative") {
+                                      webSlot.setAttribute("href", data.results[0].openstates_url)
+                                      webSlot.textContent = "Contact your " + data.results[0].current_role.title
+                                  } else if (data.results[0].current_role.title === "Senator") {
+                                      webSlot.setAttribute("href", data.results[0].openstates_url)
+                                      webSlot.textContent = "Contact your " + data.results[0].current_role.title
+                                  } else if (data.results[0].current_role.title === "Delegate") {
+                                      webSlot.setAttribute("href", data.results[0].openstates_url)
+                                      webSlot.textContent = "Contact your " + data.results[0].current_role.title
+                                  } else {
+                                      webSlot.setAttribute("href", data.results[0].openstates_url)
+                                      webSlot.textContent = "Contact!"
+                                  }
+
+                                  //List Facebook
+
+                                  var fetchFacebook = data.results[0].extras.facebook
+                                  var facebookSlot = document.getElementById("facebook-data")
+                                  if (data.results[0].extras.facebook) {
+                                      facebookSlot.setAttribute("href", fetchFacebook)
+                                  }
+
+                              })
+                      })
+              })
+      })
 }
 
 loadZips();
